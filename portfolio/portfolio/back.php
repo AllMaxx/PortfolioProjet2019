50e6f42a56cfcd5fa83c48a957ee7f2791ee4587
AllMaxx <46808659+AllMaxx@users.noreply.github.com>
2019-06-11 11:38:14 +0200

    Add files via upload

---
+++ portfolio/portfolio/back.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.0/css/all.css' integrity='sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ' crossorigin='anonymous'>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="Portfolio2.css"/>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	<meta charset="utf-8">
+	<title>Administration</title>
+</head>
+<body>
+
+	<aside>
+    <nav>
+        <img src="maxence.monnier.jpg" alt="MaxenceMONNIER">
+        </a>
+        <a href="#Administration"><i class="material-icons md-36">desktop_mac</i>
+            <p>ADMIN</p>
+        </a>
+        
+    </nav>
+    </aside>
+
+<section class="Administration" id="Administration">
+    <h6>Espace d'administration</h6>
+    <h4>Bienvenue Maxence !</h4>
+    <button class="btn1"><i class="fas fa-plus-square"></i> Ajouter du texte</button>
+    <button class="btn2"><i class="fas fa-file-image"></i> Ajouter une image</button>
+    <button class="btn3"><i class="fa fa-trash"></i> Supprimer du contenu</button>
+ 
+      
+      
+
+</section>
+
+
+	
+</body>
+</html>
